cd8ecc604fc791fa7054b1b790caf2efc3e8d275
Chris Hand <cj.hand@gmail.com>
2022-09-08 22:02:20 -0700

    Work in progress

--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
 }
 
